36bdf17d2120cd7c24b1c84766e31ec68e2f506e
vburdakovamellow <v.burdakova@mellow.io>
2026-01-28 10:44:17 +0300

    Update vite config: use port 10173 for dev server

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5000,
+    port: 10173,
+    host: "localhost",
     strictPort: false
   }
 });
